3bf611d108f8510094847b78f8ff98ad7d1825f8
Lordinp <phnasc2025@gmail.com>
2026-02-26 22:30:30 -0300

    Arquivo_inicial

---
+++ Exercicio2_Salario_AJuste.py
@@ -0,0 +1,9 @@
+#Declaração de Variável
+Salario = 0
+
+#Início
+Salario = int(input("Digite o valor do seu salário:"))
+Reajuste = (Salario*0.15)
+SCR = (Salario-Reajuste)
+print("O valor do salário reajustado é de", SCR)
+#Fim
