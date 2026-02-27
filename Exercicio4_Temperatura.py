26cc4a4de09c47b389141096d13dcf85535cb1a4
Lordinp <phnasc2025@gmail.com>
2026-02-26 23:04:59 -0300

    Arquivos_Iniciais

---
+++ Exercicio4_Temperatura.py
@@ -0,0 +1,8 @@
+from decimal import Decimal
+#Declaração de variável
+temperatura_C = 0.0
+
+temperatura_C = float(input("DIgite aqui a temperatura em Celsius:"))
+Fahrenheit = (9*temperatura_C+160)/5
+
+print("A temperatura é", Fahrenheit)
