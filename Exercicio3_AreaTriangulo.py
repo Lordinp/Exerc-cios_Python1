2636b5bc1deacdf2474718b734e59c3ffce49e9a
Lordinp <phnasc2025@gmail.com>
2026-02-26 22:55:21 -0300

    Arquivos_Adicionais

---
+++ Exercicio3_AreaTriangulo.py
@@ -0,0 +1,10 @@
+#Declarando variável
+Base = 0
+Altura = 0
+
+Base = int(input("Digite aqui o valor da base:"))
+Altura = int(input("Digite aqui o valor da altura:"))
+
+Area = (Base*Altura/2)
+
+print("O valor da área é", Area)
